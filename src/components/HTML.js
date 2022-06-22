50948f923eed180460ebaf296133ae89a626edbe
Jorge Gonzalez <jglez.cs@gmail.com>
2022-06-21 22:53:09 -0500

    Adds HTML resources

--- src/components/HTML.js
+++ src/components/HTML.js
@@ -1,7 +1,52 @@
 function HTML() {
   return(
-    <div className="topic">
-      <h2>HTML</h2>
+    <div className="topic-container">
+      
+      <h2 className="topic-heading">HTML</h2>
+
+      <ul className="resources-list">
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://divtable.com/table-styler/" target="_blank" rel="noreferrer">Table Generator</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://html.com/wp-content/uploads/html-cheat-sheet.pdf" target="_blank" rel="noreferrer">Cheat Sheet: Comprehensive Tag List and Examples</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://html-css-js.com/html/tags/" target="_blank" rel="noreferrer">Element Snippet Examples</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://www.codecademy.com/learn/learn-html/modules/learn-html-elements/cheatsheet" target="_blank" rel="noreferrer">Codecademy - Cheat Sheet</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://www.codecademy.com/learn/learn-html" target="_blank" rel="noreferrer">Codecademy - Free Course</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://www.codecademy.com/resources/docs/html" target="_blank" rel="noreferrer">Codecademy - Documentation</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML" target="_blank" rel="noreferrer">MDN - HTML Guides</a>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-angle-right"></i>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element" target="_blank" rel="noreferrer">MDN - Elements Reference</a>
+        </li>
+
+      </ul>
+
     </div>
   )
 }
